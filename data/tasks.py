0758537be904073fd6c1d1e6374c6141c2cb07b6
Zinix <romadesl21@gmail.com>
2023-03-18 01:03:31 +0300

    task model added

---
+++ data/tasks.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from sqlalchemy import orm
+from db_session import SqlAlchemyBase
+
+
+class Task(SqlAlchemyBase):
+    __tablename__ = 'tasks'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
+    question = sqlalchemy.Column(sqlalchemy.String)
+    answer = sqlalchemy.Column(sqlalchemy.String)
+    addition = sqlalchemy.Column(sqlalchemy.String)
+
+    def to_dict(self):
+        res = {
+            "id": self.id,
+            "question": self.question,
+            "answer": self.answer,
+            "addition": self.addition
+        }
+        return res
