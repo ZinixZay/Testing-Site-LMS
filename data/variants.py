617aefcc7169c57f9d2d039f3b40d79713291800
Zinix <romadesl21@gmail.com>
2023-03-18 00:57:41 +0300

    variants model added

---
+++ data/variants.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from sqlalchemy import orm
+from db_session import SqlAlchemyBase
+
+
+class Variant(SqlAlchemyBase):
+    __tablename__ = 'variants'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
+    secrecy = sqlalchemy.Column(sqlalchemy.Boolean, default=0)
+    author = orm.relationship("User", back_populates='user')
+    identifier = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    theme = sqlalchemy.Column(sqlalchemy.String)
+    task_list = sqlalchemy.Column(sqlalchemy.String)
+
+    def to_dict(self):
+        res = {
+            "id": self.id,
+            "secrecy": self.secrecy,
+            "author": self.author,
+            "identigier": self.identifier,
+            "theme": self.theme,
+            "task_list": self.task_list
+        }
+        return res
