c9ad55093a18a01dc0c5ab71c3e575c8c7affb78
Horlou <egorchernov37@yandex.ru>
2023-03-18 13:09:30 +0300

    register form template added

---
+++ lib/register_template.py
@@ -0,0 +1,11 @@
+import flask_wtf
+import wtforms
+
+
+class RegisterForm(flask_wtf.FlaskForm):
+    role = wtforms.SelectField('Вы:',
+                               choices=[('teacher', 'Учитель'), ('student', 'Ученик')])
+    login = wtforms.StringField('Логин', validators=[wtforms.validators.DataRequired()])
+    hashed_password = wtforms.PasswordField('Пароль', validators=[wtforms.validators.DataRequired()])
+    email = wtforms.StringField('Почта', validators=[wtforms.validators.DataRequired(), wtforms.validators.Email()])
+    submit = wtforms.SubmitField('Зарегистрироваться')
