c97c0745720ab6f6b9574754c51a8f58bfb9fe6a
Zinix <romadesl21@gmail.com>
2023-03-18 14:52:54 +0300

    login template reworked

--- lib/login_template.py
+++ lib/login_template.py
@@ -3,7 +3,7 @@ import wtforms
 
 
 class LoginForm(flask_wtf.FlaskForm):
-    login = wtforms.StringField('Почта или Логин', validators=[wtforms.validators.DataRequired()])
+    login = wtforms.StringField('Логин', validators=[wtforms.validators.DataRequired()])
     password = wtforms.PasswordField('Пароль', validators=[wtforms.validators.DataRequired()])
     remember_me = wtforms.BooleanField('Запомнить меня')
     submit = wtforms.SubmitField('Войти')
