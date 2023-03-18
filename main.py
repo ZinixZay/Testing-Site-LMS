f9378bf66432536b1fcd911741dd3f09ec3d86ef
Horlou <egorchernov37@yandex.ru>
2023-03-18 13:09:50 +0300

    register form route added

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import flask
 import os
 import dotenv
 from lib import login_template
+from lib import register_template
 from data import db_session
 
 
 app = flask.Flask(__name__)
+db_session.global_init("db/data.db")
 dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-db_session.global_init("db/data.db")
 
 
 @app.route('/')
 def index():
     return ''
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = register_template.RegisterForm()
+    if form.validate_on_submit():
+        return flask.redirect('/')
+    return flask.render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
