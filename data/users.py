29d9e6c6b4fe0b0a222bf604a2468753b008f7dd
Zinix <romadesl21@gmail.com>
2023-03-18 14:42:05 +0300

    users model bug fixed

--- data/users.py
+++ data/users.py
@@ -15,7 +15,7 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True)
 
     def set_password(self, password):
-        self.password = generate_password_hash(password)
+        self.hashed_password = generate_password_hash(password)
 
     def check_passord(self, password):
         return check_password_hash(self.hashed_password, password)
