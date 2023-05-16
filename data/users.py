fccbe99baf77cb5212ef51cbc2eb7f419c267c73
Horlou <egorchernov37@yandex.ru>
2023-05-16 12:47:20 +0300

    added name and surname columns

--- data/users.py
+++ data/users.py
@@ -12,6 +12,8 @@ class User(SqlAlchemyBase, UserMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, index=True)
     role = sqlalchemy.Column(sqlalchemy.String)
     login = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    surname = sqlalchemy.Column(sqlalchemy.String)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
 
     def set_password(self, password):
